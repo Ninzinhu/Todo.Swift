de5ca14043d914b866bde94180797c182ea24655
João <pel118201@gmail.com>
2024-08-01 22:58:20 -0300

    feat: add animation row

--- Tasks/ViewController.swift
+++ Tasks/ViewController.swift
@@ -2,9 +2,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+@IBOoutlet var tableView: UITableView!
+
+
   override func viewDidLoad(){
     super.viewDidLoad()
 
   }
   // Faça qualquer configuração após carregar a visualização.
+}
+
+extension ViewController: UITableViewDelegate {
+
+
+  func tableView( tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+    tableView.didSelectRow(at: indexPath, animated: true)
+  }
+
+
+
+  
+}
+
+extension ViewController: UITableViewDataSource {
+
+  
 }
